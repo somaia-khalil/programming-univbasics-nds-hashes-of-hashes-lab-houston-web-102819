69558aed7f3927a83f98f541bad830886581e358
Iam H <sumaya_khair@live.com>
2019-12-16 16:26:44 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,35 +45,6 @@
 #end
 
 
-def naming_system
-portion1 = {
-  label: "Kingdom",
-  sub_category: {
-    label: "Phylum",
-    sub_category: {
-      label: "Class",
-      sub_category: {
-       label: "Order",
-        sub_category:{
-          label: "Family",
-           sub_category: {
-            label: "Genus",
-            sub_category: {
-              label: "Species",
-              sub_category: NIL
-            }          
-  }
-
-      }
-    }
-  
-    }
-  }
-}
-
-
-
-end 
 
 
 
