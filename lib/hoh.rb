820db6ab73ee4dff78cdd947e2d22b0414672abc
Iam H <sumaya_khair@live.com>
2019-12-16 16:41:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,8 +49,22 @@
 def naming_system
 
 portion = {
-  
+  {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class",
+      sub_category: {
+        label: "Order"
+      }
+    }
+  }
 }
+
+}
+  
+
 end 
 
 
